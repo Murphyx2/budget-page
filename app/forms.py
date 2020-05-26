d24ac83e1c2443a40b900c5643a52bae1ddb98f7
Braulio Cespedes <brauliojose1@gmail.com>
2020-05-26 00:00:59 -0400

    Added some html templates and some form as an example of how to implement them

---
+++ app/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextField
+from wtforms.validators import DataRequired, Length, Email
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', [DataRequired()])
+    email = StringField('Email', [
+        Email(message=('Not a valid email address.')),
+        DataRequired()])
+    body = TextField('Message', [
+        DataRequired(), 
+        Length(min=4, message=('Your message is too short.'))])
+    recaptcha = RecaptchaField()
+    submit = SubmitField('Submit')
+
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,34 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm, ContactForm
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Home')
+nav = [{'name':'Home', 'url':'/home'},
+        {'name':'Login','url':'/login'},
+        {'name':'About','url':'/about'},
+        ]
 
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('home.html', title='Home'
+                            , description="Budget page index"
+                            , nav = nav)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form, nav = nav)
+
+
+@app.route('/about', methods=('GET', 'POST'))
+def about():
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect('/index', nav=nav, description='Algo', title='')
+    return render_template('about.html', title='About', description= 'This about the page', nav=nav, form=form)
