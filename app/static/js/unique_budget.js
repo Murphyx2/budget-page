f955a6213c607e4eaea254eec695f3fe5175fc5b
Braulio Cespedes <brauliojose1@gmail.com>
2020-07-30 00:13:12 -0400

    delete_income_expense_element html added to work on the modal. added styles to new buttons add, edit, remove on unique_budget. Added function to remove rows, I need to work in a way to remove rows income expenses elements from the database, also a modal must be done to ask the user if he/she wants to remove it on unique_budgetjs. Buttons to add, edit, remove, save and cancel were added to the unique_budgethtml, add and remove buttons works, there are some other feature the remove button should have, but that is a thing for another moment

--- app/static/js/unique_budget.js
+++ app/static/js/unique_budget.js
@@ -1,26 +1,39 @@
+var DEFAULT_ROW_HEIGHT = "2em";
+var GLOBAL_NUMBER_ROW  = 0;
 
 function create_newrow(tableName){
 
     var MAX_CELL_NUMBER = 4;
     var TAG_ID_ELEMENTS = [tableName+"_name", tableName+"_planned_amount", tableName+"_actual_amount", tableName+"_difference"]
-    var COUNT_EXPENSE_TABLE_ELEMENTS = document.getElementById(tableName+'Table').getElementsByTagName('tbody').length -1;
+    var COUNT_ROWS_TABLE_ELEMENTS = document.getElementById(tableName+'Table').getElementsByTagName('tbody').length -1;
 
-    var table = document.getElementById(tableName+'Table').getElementsByTagName('tbody')[COUNT_EXPENSE_TABLE_ELEMENTS];
+    var table = document.getElementById(tableName+'Table').getElementsByTagName('tbody')[COUNT_ROWS_TABLE_ELEMENTS];
     var last_row_number = table.rows.length;
     var newRow = table.insertRow(last_row_number);
     newRow.setAttribute("id", tableName +"Row"+ (last_row_number + 1));
+    newRow.style.height = DEFAULT_ROW_HEIGHT;
 
     for (var count = 0; count < MAX_CELL_NUMBER ; count++) {
             var newCell = newRow.insertCell(count);
             newCell.setAttribute("id",TAG_ID_ELEMENTS[count] + (last_row_number+1));    
     }
+    GLOBAL_NUMBER_ROW++;
     
-
 }
 
-create_newrow("expense");
-
-
+function remove_lastrow(tableName){
+    var COUNT_ROWS_TABLE_ELEMENTS = document.getElementById(tableName+'Table').getElementsByTagName('tbody').length -1;
+    var table = document.getElementById(tableName+'Table').getElementsByTagName('tbody')[COUNT_ROWS_TABLE_ELEMENTS];
+    var last_row_number = table.rows.length - 1;
+    //At the moment you cannot delete existing elements in the table
+    if(GLOBAL_NUMBER_ROW>0){
+        table.deleteRow(last_row_number)
+        GLOBAL_NUMBER_ROW--;
+    }else if(GLOBAL_NUMBER_ROW === 0){
+        //I should ask to the user if he wants to eleminate 
+        console.log("Modal to ask to delete an elemented in the database");
+    }
+}
 
 
 
